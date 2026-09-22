10c6aca9ac2c97a40c679997cdcddc63784944c4
Onyiobazi <yireagroup@gmail.com>
2026-09-22 21:41:44 +0100

    chore: cleanup mjs files

--- patch_shop_build2.mjs
+++
@@ -1,9 +0,0 @@
-﻿import fs from "fs";
-let s = fs.readFileSync("components/retail/ShopDashboard.tsx", "utf8");
-
-s = s.replace(
-  'locationId: operatingLocationId!',
-  'locationId: ""'
-);
-
-fs.writeFileSync("components/retail/ShopDashboard.tsx", s);
--- patch_shop_build3.mjs
+++
@@ -1,9 +0,0 @@
-﻿import fs from "fs";
-let s = fs.readFileSync("components/retail/ShopDashboard.tsx", "utf8");
-
-s = s.replace(
-  'await updatePurchaseOrderStatus(po.id, next[po.status] as "DRAFT" | "SENT" | "RECEIVED" | "PARTIAL");',
-  'await updatePurchaseOrderStatus(po.id, next[po.status] as any);'
-);
-
-fs.writeFileSync("components/retail/ShopDashboard.tsx", s);
